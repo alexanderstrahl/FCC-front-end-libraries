7bfcdd03d82a2d07f6c34cbdfbe22ea4b24253c9
astrahl008 <alexanderstrahl@hotmail.com>
2020-10-03 08:02:14 -0400

    finished react 28

---
+++ React/react-28.js
@@ -0,0 +1,29 @@
+//React: Create a Controlled Input
+class ControlledInput extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: ''
+      };
+    }
+    // change code below this line
+    handleChange(event) {
+      this.setState({input: event.target.value})
+    }
+    // change code above this line
+    render() {
+      return (
+        <div>
+          { /* change code below this line */}
+          <input onChange = 
+            {this.handleChange.bind(this)} 
+            value = {this.state.input}
+          />
+          { /* change code above this line */}
+          <h4>Controlled Input:</h4>
+          <p>{this.state.input}</p>
+        </div>
+      );
+    }
+  };
+  
