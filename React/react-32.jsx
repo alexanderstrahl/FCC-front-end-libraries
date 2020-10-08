2486d98624588a80373ed1d79c8b0dae61fa48b7
astrahl008 <alexanderstrahl@hotmail.com>
2020-10-07 23:41:02 -0400

    react 32

---
+++ React/react-32.jsx
@@ -0,0 +1,15 @@
+//React: Use the Lifecycle Method componentWillMount
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    componentWillMount() {
+      // change code below this line
+      console.log("Component Will Mount")
+      // change code above this line
+    }
+    render() {
+      return <div />
+    }
+  };
+  
