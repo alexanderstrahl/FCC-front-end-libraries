1e5745ccbf868027c18edfedf7cd36121e660590
astrahl008 <alexanderstrahl@hotmail.com>
2020-10-23 12:52:57 -0400

    react 37

---
+++ React/react-37.jsx
@@ -0,0 +1,12 @@
+//React: Add Inline Styles in ReactPassed
+var styles = {color:"purple", fontSize:40, border:"2px solid purple"}
+// Change code above this line
+class Colorful extends React.Component {
+  render() {
+    // Change code below this line
+    return (
+      <div style={styles}>Style Me!</div>
+    );
+    // Change code above this line
+  }
+};
