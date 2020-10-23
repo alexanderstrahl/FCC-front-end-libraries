7cef2de53a0d2a6c2dfeaf87b0e9671ba323d654
astrahl008 <alexanderstrahl@hotmail.com>
2020-10-23 12:42:47 -0400

    react 36

---
+++ React/react-36.jsx
@@ -0,0 +1,8 @@
+//React: Introducing Inline Styles
+class Colorful extends React.Component {
+    render() {
+      return (
+        <div style={{color: "red", fontSize: 72}}>Big Red</div>
+      );
+    }
+  };
