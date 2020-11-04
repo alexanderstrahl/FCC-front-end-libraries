b559223dbdc38557edf0c426ad3faf63cc0068a4
astrahl008 <alexanderstrahl@hotmail.com>
2020-11-04 18:14:33 -0500

    react 46

---
+++ React/react-46.jsx
@@ -0,0 +1,45 @@
+//React: Use Array.filter() to Dynamically Filter an Array
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        users: [
+          {
+            username: 'Jeff',
+            online: true
+          },
+          {
+            username: 'Alan',
+            online: false
+          },
+          {
+            username: 'Mary',
+            online: true
+          },
+          {
+            username: 'Jim',
+            online: false
+          },
+          {
+            username: 'Sara',
+            online: true
+          },
+          {
+            username: 'Laura',
+            online: true
+          }
+        ]
+      };
+    }
+    render() {
+      const usersOnline = this.state.users.filter(user => user.online == true); // Change this line
+      const renderOnline = usersOnline.map(i => <li key={i.username}>{i.username}</li>); // Change this line
+      return (
+        <div>
+          <h1>Current Online Users:</h1>
+          <ul>{renderOnline}</ul>
+        </div>
+      );
+    }
+  }
+  
