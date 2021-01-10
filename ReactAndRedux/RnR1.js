dac4e65473d92e1dab5184fad18f7001cbb698c6
astrahl008 <alexanderstrahl@hotmail.com>
2021-01-10 13:01:58 -0500

    Finished Redux section and started React&Redux

---
+++ ReactAndRedux/RnR1.js
@@ -0,0 +1,15 @@
+//React and Redux: Getting Started with React Redux
+class DisplayMessages extends React.Component {
+    // Change code below this line
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: '',
+        messages: []
+      }
+    }
+    // Change code above this line
+    render() {
+      return <div />
+    }
+  };
